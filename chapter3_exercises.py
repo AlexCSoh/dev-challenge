ac89f127badfaf7af7a2377088082c6507ce8caf
AlexCSoh <schau1419@yahoo.com>
2014-06-12 19:59:17 -0400

    the box

--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,29 @@
 # Exercises for chapter 3:
 
+#Ex 3.5
+
+def print_box():
+	part1 = "+ - -"
+	part2 = "- -"
+	part3 = "+"
+	part4 = "|        "
+	part5 = "|"
+
+	print part1,part2,part1,part2,part3
+	print part4,part4,part5
+	print part4,part4,part5
+	print part4,part4,part5
+	print part4,part4,part5
+	print part1,part2,part1,part2,part3
+	print part4,part4,part5
+	print part4,part4,part5
+	print part4,part4,part5
+	print part4,part4,part5
+	print part1,part2,part1,part2,part3
+
+print print_box
+
+print_box()
+
+
 # Name:
